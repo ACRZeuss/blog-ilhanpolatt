03fa57ef7741538f41bf4e84f4ff0b31fb27d11b
ACRZeuss <69459810+ACRZeuss@users.noreply.github.com>
2022-02-26 15:48:57 +0300

    Dark Mode Eklendi

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { FaInstagram, FaYoutube } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className='bg-gray-900 text-gray-100 shadow w-full'>
+    <header className='header bg-gray-900 text-gray-100 shadow w-full'>
       <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
         <Link href='/'>
           <a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
